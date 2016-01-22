659cf4d04fd5df6f1c95c1d375e6f3eac41bdf78
Ronando Long <ronando.long@gmail.com>
2016-01-21 19:43:06 -0800

    delete

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 // Business Logic
+var pingPong = function(number1, number2){
+  var ping = "ping";  // scaleable: in case dev/manager/user wants to change output
+  var pong = "pong";  // same as above
+  var num1 = number1; // Scaleable: in case dev/manager/user wants to change input
+  var num2 = number2; // same as above
 
+  if (isNaN(num1) || isNaN(num2)) {  // error handling for words and/or empty input
+    return "error message";  // define 'error message' in Interface Logic section
+  } else if () {
+
+  }
+
+
+
+};
 
 
 
