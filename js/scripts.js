340e488f63ea9ddd8af19722bade7d778c7be1ff
Ronando Long <ronando.long@gmail.com>
2016-01-21 23:52:23 -0800

    It will replace positive and negative multiples of 3 and 5 with 'ping' and 'pong' as well as error handle NaN.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,61 @@
 // Business Logic
 
+// Error Handling
+var errorEntry = function(number) {
+  var output = [];
 
+  if (isNaN(number) || number === "") {  // if non number or empty entry is inputted
+    output.push("error message");
+  } return output;
+};  // errorEntry
 
+// Main Logic
+var pingPong = function(number) {
+  // variables to change output easier. Boss might want diff numbers, & words other than ping & pong
+  var ping = "ping";
+  var pong = "pong";
+  var number1 = 3;
+  var number2 = 5;
+  var output = [];
+
+  for (var i = 1; i <= number; i++) {
+    if (i % number1 === 0 && i % number2 === 0) {
+      output.push(ping + pong);
+    } else if (i % number1 === 0) {
+      output.push(ping);
+    } else if (i % number2 === 0) {
+      output.push(pong);
+    } else {
+      output.push(i);
+    }
+  }
+  return output;
+};  // pingPong
+
+// Negative Numbers
+var negativeNumber = function(number) {
+  var ping = "ping";
+  var pong = "pong";
+  var number1 = 3;
+  var number2 = 5;
+  var negativeOutput = [];
+
+  if (number < 0) {
+    for (var i = -1; i >= number; i--) {
+      if (i % number1 === 0 && i % number2 === 0) {
+        negativeOutput.push(ping + pong);
+      } else if (i % number1 === 0) {
+        negativeOutput.push(ping);
+      } else if (i % number2 === 0) {
+        negativeOutput.push(pong);
+      } else {
+        negativeOutput.push(i);
+      }
+    }
+  }
+console.log(negativeOutput);
+  return negativeOutput;
+};  // negativeNumber
 
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,4 +1,30 @@
+describe('errorEntry', function(){
+  it("will result in error message with text input", function(){
+    expect(errorEntry("f")).to.eql(["error message"]);
+  });
+
+  it("will result in error message with empty input", function(){
+    expect(errorEntry("")).to.eql(["error message"]);
+  });
+});
+
 describe('pingPong', function(){
-  
+  it("will replace multiples of 3 with ping", function(){
+    expect(pingPong(6)).to.eql([1, 2, "ping", 4, "pong", "ping"]);
+  });
+
+  it("will replace multiples of 5 with 'pong'", function(){
+    expect(pingPong(10)).to.eql([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong"]);
+  });
+
+  it("will replace 3 with ping and 5 with pong.", function(){
+    expect(pingPong(15)).to.eql([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong"]);
+  });
+});
+
+describe('negativeNumber', function() {
+  it("will replace negative multiples of 3 and 5 with ping and pong", function() {
+    expect(negativeNumber(-15)).to.eql([-1, -2, "ping", -4, "pong", "ping", -7, -8, "ping", "pong", -11, "ping", -13, -14, "pingpong"]);
+  });
 
 });
