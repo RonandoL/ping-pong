54a086a598cd253fd25cebc9ef67f53b1d471160
Ronando Long <ronando.long@gmail.com>
2016-01-21 19:31:41 -0800

    Business: It will output error message for isNaN.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,23 @@
 // Business Logic
-var pingPong = function(number){
-  if (number === 3) {
-    return "ping"
-  } else if (number === 5) {
-    return "pong"
-  } else if (true) {
+var pingPong = function(number1, number2){
+  var ping = "ping";
+  var pong = "pong";
+  var num1 = number1;
+  var num2 = number2;
+
+  if (isNaN(num1) || isNaN(num2)) {
+    return "error message";
+  } else {
 
   }
+
+  // if (num1 %) && (num2 === 5) {
+  //   return ("ping"
+  // } else if (number2 === 5) {
+  //   return "pong"
+  // } else if (true) {
+  //
+  // }
 
 };
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,15 +1,15 @@
 describe('pingPong', function(){
-  it("will replace 3 with 'ping'", function(){
-    expect(pingPong(3)).to.equal("ping");
+  it("will handle a non number with a message'", function(){
+    expect(pingPong("f", "")).to.equal("error message");
   });
 
-  it("will replace 5 with 'pong'", function(){
-    expect(pingPong(5)).to.equal("pong");
-  });
+  // it("will replace 3 with 'ping'", function(){
+  //   expect(pingPong(3, 5)).to.equal("ping", "pong");
+  // });
 
-  it("will replace multiples of 3 x 5 with 'pingpong'", function(){
-    expect(pingPong(15)).to.equal("pingpong");
-  });
+  // it("will replace multiples of 3 x 5 with 'pingpong'", function(){
+  //   expect(pingPong(15)).to.equal("pingpong");
+  // });
 
   // it("is going to count up to an entered number", function(){
   //   expect(pingPong(7)).to.equal(1, 2, 3, 4, 5, 6, 7);
